e0df2580526b535887008f45ca009eb1a42f574d
HarishaHaridas <harisha3004@gmai.com>
2023-10-05 18:14:28 -0400

    InlineStyle removed

--- src/screens/LoadingScreen.tsx
+++ src/screens/LoadingScreen.tsx
@@ -1,21 +1,24 @@
 import * as React from 'react';
-import {ActivityIndicator, StyleSheet, Text, View} from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
 
 const LoadingScreen = () => {
   return (
     <View style={styles.container}>
-      <Text style={{fontSize: 20, color: '#000000'}}>Loading.....</Text>
-      <ActivityIndicator color={"#0000ff"} size={'large'}/>
+      <Text style={styles.subContainer}>Loading.....</Text>
+      <ActivityIndicator color={"#0000ff"} size={'large'} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        paddingHorizontal: 0,
-        backgroundColor: '#fafafa'
-      },
- });
+  container: {
+    flex: 1,
+    paddingHorizontal: 0,
+    backgroundColor: '#fafafa'
+  },
+  subContainer: { 
+    fontSize: 20, 
+    color: '#000000' }
+});
 
 export default LoadingScreen;
